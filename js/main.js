510753a7836b61ca47f20a0c7816919c6376ca90
som-syom <dhyeon@student.42seoul.kr>
2021-08-17 01:58:10 +0900

    Add - notice promotion 스와이퍼 추가

--- js/main.js
+++ js/main.js
@@ -51,3 +51,46 @@ fadeEls.forEach(function (fadeEl, index) {
 		opacity: 1
 	});
 });
+
+// ------------------------- 스와이퍼
+// Swiper(선택자, 옵션)
+new Swiper('.notice-line .swiper-container', {
+	direction: 'vertical',
+	autoplay: true,
+	loop: true
+});
+
+new Swiper('.promotion .swiper-container', {
+	// direction: 'horizontal', 기본값임
+	slidesPerView: 3, //한번에 보여줄 슬라이드 개수 기본값은 1
+	spaceBetween: 10, // 슬라이드 사이 여백
+	centeredSlides: true, // 1번 슬라이드가 가운데 보이기
+	loop: true,
+	// autoplay: {
+	// 	delay: 5000
+	// },
+	pagination: {
+		el: '.promotion .swiper-pagination', // 페이지 번호 요소 선택자
+		clickable: true // 사용자의 페이지 번호 요소 제어 가능 여부
+	},
+	navigation: {
+		prevEl: '.promotion .swiper-prev',
+		nextEl: '.promotion .swiper-next'
+	}
+});
+
+
+// -------------------------- 버튼누르면 숨기고 보이게 하기
+const promotionEl = document.querySelector('.promotion');
+const promotionToggleBtn = document.querySelector('.toggle-promotion');
+let isHidePromotion = false;
+promotionToggleBtn.addEventListener('click', function () {
+	isHidePromotion = !isHidePromotion
+	if (isHidePromotion) {
+		// 숨김 처리
+		promotionEl.classList.add('hide');
+	} else {
+		// 보이게 처리
+		promotionEl.classList.remove('hide');
+	}
+})
